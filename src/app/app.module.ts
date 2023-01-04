408b9da94eb067daf05a81479f5f2726250e9a08
david2242 <88514735+david2242@users.noreply.github.com>
2023-01-04 23:31:27 +0100

    generated add-record component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRecordComponent } from './add-record/add-record.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecordComponent
   ],
   imports: [
     BrowserModule,
