7b3b2114d48ce8673f537f6cf7ed11775af69768
david2242 <88514735+david2242@users.noreply.github.com>
2023-11-26 16:11:29 +0100

    Indtroduce hu locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -64,7 +64,12 @@ import {NgxScannerQrcodeModule} from "ngx-scanner-qrcode";
     NgxKjuaModule,
     NgxScannerQrcodeModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'hu-HU'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeHu from '@angular/common/locales/hu'
+registerLocaleData(localeHu, 'hu');
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
